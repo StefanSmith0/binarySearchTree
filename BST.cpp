6a6d2ead6fe3d9ac317ce7ce578b0c9b8e88e94c
StefanSmith0 <94428283+Beldard@users.noreply.github.com>
2022-04-20 19:02:47 -0700

    added buggy, illegible garbage

--- BST.cpp
+++ BST.cpp
@@ -15,9 +15,10 @@ void Binary_tree::insert(int newInt) {
   insertNode(root, newNode);
 }
 
-node* Binary_tree::findPreviousNode(int & searchValue, node* & result, node* & prevResult) {
+node* Binary_tree::findPreviousNode(int searchValue, node* result, node* prevResult) {
   if(result) {
     if(result->value == searchValue) {
+      cout << "findPreviousNode - prevResult is: " << prevResult->value << endl;
       return prevResult;
     }
     else if(result->value > searchValue) {
@@ -32,15 +33,25 @@ node* Binary_tree::findPreviousNode(int & searchValue, node* & result, node* & p
   }
 }
 
-void Binary_tree::findNode(int& searchValue, node*& result, node*& prevResult) {
+void Binary_tree::findNode(int & searchValue, node* & result, node* & prevResult) {
   prevResult = findPreviousNode(searchValue, result, prevResult);
   if(prevResult) {
+    cout << "findNode - prevResult is: " << prevResult->value << ", finding result..." << endl;
     if(prevResult->lchild->value == searchValue) {
+      cout << "findNode - Result is lchild of prevResult" << endl;
       result = prevResult->lchild;
     }
-    else {
+    else if(prevResult->rchild->value == searchValue) {
+      cout << "findNode - Result is rchild of prevResult" << endl;
       result = prevResult->rchild;
     }
+    else {
+      cout << "findNode - couldn't find result from prevResult, returning root..." << endl;
+      result = root;
+    }
+  }
+  else {
+    cout << "findNode - What the fuck" << endl;
   }
 }
 
@@ -55,6 +66,7 @@ void Binary_tree::remove(int deleteValue) {
   node* result = root;
   node* prevResult = root;
   findNode(deleteValue, result, prevResult);
+  cout << "remove - Target locked: " << result->value << endl;
   bool lchild = islchild(result, prevResult);
   if(!result->lchild && !result->rchild) { //leaf
     if(lchild) {
@@ -63,16 +75,40 @@ void Binary_tree::remove(int deleteValue) {
     }
   }
   else if(result->lchild && result->rchild) { //two children
-    node* replacement = result->rchild;
-    node* replacementL = replacement->lchild;
-    while(replacementL != nullptr) {
-      replacement = replacementL;
-      replacementL = replacement->lchild;
+    cout << "remove - Node has two children." << endl;
+    node* replacementParent = result->rchild;
+    node* replacement = replacementParent->lchild;
+    cout << "remove - Finding leftmost of right substree..." << endl;
+    if(replacement) {
+      while(replacement->lchild) { //find leftmost of right subtree
+	replacementParent = replacement;
+	replacement = replacementParent->lchild;
+      }
+      cout << "remove - replacementParent is: " << replacementParent->value << endl;
+      cout << "remove - replacement is: " << replacement->value << endl;
+    }
+    cout << "Last left of right found: " << replacement->value << ", swapping value to: " << result->value << endl;
+    if(replacement->rchild) {
+      if(replacementParent == result) {
+	replacementParent->rchild = replacement->rchild;
+      }
+      else {
+	cout << "remove - replacement has rchild, moving rchild upward..." << endl;
+	replacementParent->lchild = replacement->rchild;
+      }
+    }
+    else {
+      if(replacementParent == result) {
+	replacementParent->rchild = nullptr;
+      }
+      else {
+	replacementParent->lchild = nullptr;
+      }
     }
     int replacementValue = replacement->value;
-    cout << "sawg" << replacementValue << endl;
-    remove(replacementValue);
-    result->value = replacement->value;
+    delete replacement;
+    result->value = replacementValue;
+    cout << "remove - Success" << endl;
   }
   else if(result->lchild || result->rchild) { //one child
     node* child;
@@ -120,6 +156,9 @@ void Binary_tree::inorder(node* & root) {
 
 //Prints the tree as a horizontal tree - printBT by Adrian Schneider (modified)
 void Binary_tree::displayTree(bool isLeft, node* root, const string &prefix) {
+  if(prefix.length() > 100) {
+    return;
+  }
   if(root) {
     cout << prefix;
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "BST.h"
 #include "time.h"
+#include <cstring>
 
 using namespace std;
 
@@ -10,12 +11,28 @@ int main() {
   for(int i = 0; i < 10; i++) {
     bst->insert((rand() % 999) + 1);
   }
+  char input[10];
+  bool running = true;
   bst->display();
-  int input;
-  cout << "Choose a number to remove: ";
-  cin >> input;
-  bst->remove(input);
-
-  bst->display();
+  while(running) {
+    cout << endl;
+    cout << "Command: ";
+    cin.getline(input, 10);
+    if(!strcmp(input, "rm")) {
+      int intInput = 0;
+      cout << "Choose a number to remove: ";
+      cin >> intInput;
+      bst->remove(intInput);
+      cin.ignore();
+      cin.clear();
+    }
+    else if(!strcmp(input, "d")) {
+      bst->display();
+    }
+    else if(!strcmp(input, "q")) {
+      running = false;
+    }
+    cout << endl;
+  }
   return 0;
 }
